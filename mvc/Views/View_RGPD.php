e57f8306ca81d6ef04ea08a65a8217e287302830
mohamed-ndoye811 <NdoyeMD@outlook.fr>
2021-09-09 10:53:35 +0200

    Ajout view

---
+++ mvc/Views/View_RGPD.php
@@ -0,0 +1,34 @@
+<main class="rgpd">
+    <h1>Qui sommes-nous ?</h1>
+    <p>Le Greta du Var est de centre de formation dont l’adresse du site internet est http://www.greta.ac-nice.fr/ . Le
+        responsable de traitement des données personnelles et délégué à la protection des données est Muriel REMISE que
+        vous
+        pouvez contacter à l’adresse mail suivante : muriel.remise@gmail.com. </p>
+
+    <h1>Utilisation des données personnelles collectées</h1>
+    <p>Le Greta du Var collecte les noms et prénoms des stagiaires/étudiants, leur adresse, leur e-mail, leur numéro de
+        téléphone, leur date de naissance, leur parcours de formation et professionnel, leur photographie. Les
+        informations dites lors des entretiens peuvent aussi être enregistrées lorsque celles-ci sont utiles à la
+        formation. Cependant aucunes données sensibles ne seront enregistrées sans l’accord écrit du stagiaire/étudiant.
+        Après consentement du stagiaire/étudiant, ces informations sont récoltées et stockées un an à compter de
+        l’inscription du stagiaire/étudiant afin de pouvoir inscrire le stagiaire/étudiant à la formation souhaitée, le
+        contacter quand cela est nécessaire pour la formation et l’aider au mieux tout au long de la formation.
+        Le traitement de vos données personnelles peut également être imposé par des dispositions légales ou
+        réglementaires, telles que :
+    <ul>
+        <li>L’obligation de transmettre des données vous concernant aux organismes de protection sociale afin qu’ils
+            déterminent vos droits sociaux</li>
+
+        <li>
+            L’obligation de transmettre tout document afférent aux prestations dont vous avez bénéficié dans le cadre du
+            contrôle des financements mobilisés.
+        </li>
+    </ul>
+
+    Votre adresse IP, l’agent utilisateur de votre navigateur ainsi que l’heure de votre connexion sont récoltées
+    lors d’envoi des formulaires de contact, de commentaires et témoignages afin de nous aider à détecter ceux qui
+    sont indésirables.
+    Les informations concernant les financements des formations (carte bancaire, compte CPF) ne sont pas
+    enregistrées. Elles ne sont en aucun cas transmises à un tiers.
+    </p>
+</main>
